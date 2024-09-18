d41091ef30a82454297d944424a5f2ae2dfd9b55
dreya0001 <andreya.colvin@snhu.edu>
2024-09-18 12:29:30 -0700

    Added project files to repository

---
+++ app_server/controllers/travel.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('.data/trips.json','utf8'));
+
+/* GET travel view */
+const travel = (reg, res) => {
+    res.render('travel', { title: 'Travlr Getaways'});
+};
+module.exports = {
+    travel
+};
