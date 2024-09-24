eb33093c57c0c5e4e8567331369486160252c26e
dreya0001 <andreya.colvin@snhu.edu>
2024-09-24 11:23:37 -0700

    Added files from travlr project

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ var logger = require('morgan');
 
 var indexRouter = require('./app_server/routes/index'); // Updated path to include app_server
 var usersRouter = require('./app_server/routes/users'); // Updated path to include app_server
+var travelRouter = require('./app_server/routes/users'); // Updated path to include app_server
+var handlebars = require('hbs');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server/views')); // Update views path to app_server/views
+
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -21,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
